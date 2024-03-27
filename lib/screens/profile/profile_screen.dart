a0c941254d333a7656e8b48bfc14cb45a491e5b8
Yorq1nnur <abduraimovyorqinjon4@gmail.com>
2024-03-27 07:07:13 +0500

    ADD PUSH NOTIFICATION

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -2,6 +2,7 @@ import 'package:e_commerce_app/utils/styles/app_text_style.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
 
 import '../../view_models/auth_view_model.dart';
@@ -31,6 +32,74 @@ class _ProfileScreenState extends State<ProfileScreen> {
               color: Colors.black,
               fontWeight: FontWeight.w900,
             ),
+          ),
+          backgroundColor: Colors.white,
+        ),
+        body: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: 20.w,
+            vertical: 20.h,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              user?.photoURL != null
+                  ? Center(
+                      child: Container(
+                        margin: EdgeInsets.only(
+                          bottom: 30.h,
+                        ),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(
+                            50.r,
+                          ),
+                          border: Border.all(
+                            color: Colors.yellowAccent,
+                            width: 2.w,
+                          ),
+                        ),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(
+                            50.r,
+                          ),
+                          child: Image.network(
+                            user!.photoURL!,
+                            width: 200.w,
+                            height: 200.h,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    )
+                  : Center(
+                      child: Container(
+                        margin: EdgeInsets.only(
+                          bottom: 30.h,
+                        ),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(
+                            50.r,
+                          ),
+                          border: Border.all(
+                            color: Colors.yellowAccent,
+                            width: 2.w,
+                          ),
+                        ),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(
+                            50.r,
+                          ),
+                          child: Image.network(
+                            "https://www.tenforums.com/attachments/tutorials/146359d1501443008-change-default-account-picture-windows-10-a-user.png",
+                            width: 200.w,
+                            height: 200.h,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    ),
+              
+            ],
           ),
         ),
       ),
